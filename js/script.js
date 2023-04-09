57c5cede7fcaf0771f46f60e167b0798c511fe19
BerrieK32 <r0834599@student.thomasmore.be>
2023-04-09 14:04:00 +0200

    Declareren form waardes

--- js/script.js
+++ js/script.js
@@ -2,14 +2,28 @@
 let _errorsArray = new Array();
 
 // Validatie functie form
-function validateForm(){
-    // Declareren alert boxen
-    let foutmelding =document.getElementById("foutmelding");
-    let volledigmelding =document.getElementById("volledigmelding");
-    let betaaldmelding =document.getElementById("betaaldmelding");
+function validateForm() {
+  // Declareren alert boxen
+  let foutmelding = document.getElementById("foutmelding");
+  let volledigmelding = document.getElementById("volledigmelding");
+  let betaaldmelding = document.getElementById("betaaldmelding");
 
-    // Leeg maken van alert boxen
-    foutmelding.innerHTML = "";
-    volledigmelding.innerHTML = "";
-    betaaldmelding.innerHTML = "";
+  // Declareren form waardes
+  let voornaamInput = document.getElementById("IPvoornaam").value;
+  let naamInput = document.getElementById("IPnaam").value;
+  let gebruikersnaamInput = document.getElementById("IPgebruikersnaam").value;
+  let emailInput = document.getElementById("IPmail").value;
+  let wachtwoordInput = document.getElementById("IPwachtwoord").value;
+  let hwachtwoordInput = document.getElementById("IPhwachtwoord").value;
+  let adresInput = document.getElementById("IPadres").value;
+  let landSelect = document.getElementById("IPland").value;
+  let provincieSelect = document.getElementById("IPprovincie").value;
+  let postcodeInput = document.getElementById("IPpostcode").value;
+  let ophoogteCheck = document.getElementById("IPinschrijven").value;
+  let betalingCheck = document.querySelector('input[name="paymentMethod"]:checked').value;
+
+  // Leeg maken van alert boxen
+  foutmelding.innerHTML = "";
+  volledigmelding.innerHTML = "";
+  betaaldmelding.innerHTML = "";
 }
