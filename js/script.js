cd7ae3b97899704a310b25e9ba306c8f79561b78
BerrieK32 <r0834599@student.thomasmore.be>
2023-04-09 13:51:53 +0200

    Alert boxen aanpassing in index + start alert boxen in js

--- js/script.js
+++ js/script.js
@@ -3,5 +3,13 @@ let _errorsArray = new Array();
 
 // Validatie functie form
 function validateForm(){
+    // Declareren alert boxen
+    let foutmelding =document.getElementById("foutmelding");
+    let volledigmelding =document.getElementById("volledigmelding");
+    let betaaldmelding =document.getElementById("betaaldmelding");
 
+    // Leeg maken van alert boxen
+    foutmelding.innerHTML = "";
+    volledigmelding.innerHTML = "";
+    betaaldmelding.innerHTML = "";
 }
